17177c947ba17c03065190ba5843f2cb313a0b36
Atik-Hasan-55 <atik508030@gmail.com>
2026-09-19 20:59:47 +0600

    foots complidt card

--- src/app/Components/FootsCard.jsx
+++ src/app/Components/FootsCard.jsx
@@ -10,7 +10,7 @@ const FootsCard = ({foots}) => {
   </figure>
   <div className="card-body">
     <h2 className="card-title">{dish_name}</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <p>{category}</p>
     <div className="card-actions justify-end">
       <button className="btn btn-primary">Buy Now</button>
     </div>
--- src/app/Foots/loading.jsx
+++ src/app/Foots/loading.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 const loading = () => {
     return (
-       <span className="loading loading-dots loading-xl"></span>
+        <div>
+           <div className="min-h-screen flex items-center justify-center">
+  <span className="loading loading-dots loading-xl"></span>
+</div>
+        </div>
     );
 };
 
